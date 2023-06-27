6da913e1f29ab6dbbaf2c70ebe0e0b768121313c
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2023-06-28 00:06:58 +0200

    Basic classess

--- pyrlament/counter.py
+++ pyrlament/counter.py
@@ -1,2 +1,13 @@
+from typing import List
+
+from pyrlament.data import Party, Election
+
+
 class SeatsCounter:
-    pass
+    election: Election
+
+    def __init__(self, parties: List[Party]):
+        self.election = Election(parties=parties)
+
+    def count(self):
+        pass
--- pyrlament/data.py
+++ pyrlament/data.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 class Party(BaseModel):
     name: str
     votes: float
+    seats: Optional[int]
 
 
 class Election(BaseModel):
