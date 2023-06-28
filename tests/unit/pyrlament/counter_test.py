ab97bfa42733e459a97d9f01bff290f9b4744a98
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2023-06-28 10:21:36 +0200

    Wybory 2019

--- tests/unit/pyrlament/counter_test.py
+++ tests/unit/pyrlament/counter_test.py
@@ -10,4 +10,21 @@ class TestSeatsCounter:
         assert election.parties[0].seats == DEPUTIES
 
     def test_count_2019_election(self):
-        pass
+        parties = [
+            Party(name="PiS", votes=43.59),
+            Party(name="KO", votes=27.4),
+            Party(name="SLD", votes=12.56),
+            Party(name="PSL", votes=8.55),
+            Party(name="KWIN", votes=6.81),
+        ]
+        election = SeatsCounter(parties=parties)
+        election.count()
+        expected = {
+            "PiS": 235,
+            "KO": 134,
+            "SLD": 49,
+            "PSL": 30,
+            "KWIN": 11,
+        }
+        for party in election.parties:
+            assert party.seats == expected[party.name]
